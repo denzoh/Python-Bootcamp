1392c84d477f1adb12ad16926fa042d6b4becc91
Dennoh <dennisndungu68@gmail.com>
2018-09-08 00:50:31 +0300

    some wierd errors though

--- wordcount/forms.py
+++ wordcount/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 
 class SignupForm(forms.Form):
-    #name = forms.CharField(widget=forms.NameInput(attrs={'placeholder':'enter name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'john doe'}))
     email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder':'johndoe@gmail.com'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'************'}))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'************'}))
+    # class Meta:
+    #     model = Createuser
+    #     fields = ('username','email','password','confirm_password',)
 
 class LoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder':'johndoe@gmail.com'}))
--- wordcount/urls.py
+++ wordcount/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('',views.homepage,name = 'home'),
     path('eggs',views.eggs, name = 'eggs'),
     path('signup',views.signup, name = 'signup'),
